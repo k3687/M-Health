7cb6d6b1f7ab7fdf8066affc5a943bcfe8831328
ravindravasave <101638829+ravindravasave@users.noreply.github.com>
2022-03-16 10:30:20 +0530

    Update User.java

--- User.java
+++ User.java
@@ -3,6 +3,7 @@ package com.cg.model;
 public class User {
   public static void main(String[]args){
   System.out.println("HEllo");
+      System,out.println("Group sharing is working of project")
   }
 
 }
