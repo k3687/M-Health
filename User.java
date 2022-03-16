bbf585ccb01e406ea02ddb48a668bc3485cb5cb1
Supreethakharvi99 <101662949+Supreethakharvi99@users.noreply.github.com>
2022-03-16 10:46:45 +0530

    Update User.java

--- User.java
+++ User.java
@@ -3,11 +3,12 @@ package com.cg.model;
 public class User {
   public static void main(String[]args){
   System.out.println("HEllo");
-      System,out.println("Group sharing is working of project")
-        System.out.println("HEllo");
+      System,out.println("Group sharing is working of project");
+      System.out.println("mHealth");  
+    System.out.println("HEllo");
+
+    
+
   }
-  
-  public void userStories(){
-System.out.println("connect to mHealth for easy access  indias best health consultances");
 
 }
